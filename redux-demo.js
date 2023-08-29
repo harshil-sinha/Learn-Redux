d8995dd1cc9e4e6e42ce770ab494f96c83cfd64a
harshil-sinha <130955042+harshil-sinha@users.noreply.github.com>
2023-08-30 00:32:48 +0530

    Update redux-demo.js

--- redux-demo.js
+++ redux-demo.js
@@ -1,10 +1,19 @@
 const redux = require('redux');
 
-const counterReducer = (state = {counter : 0},action) =>{
-    return {
-        counter : state.counter+1
+const counterReducer = (state = {counter : 1},action) =>{
+    if(action.type==='increment'){
+        return {
+            counter : state.counter+1
+        }
     }
+    else if(action.type==='decrement'){
+        return {
+            counter : state.counter-1
+        }
+    }
+    return state;
 }
+
 const store = redux.createStore(counterReducer);
 
 const counterSubscriber = () =>{
@@ -12,5 +21,6 @@ const counterSubscriber = () =>{
      console.log(latestStore);
 }
 store.subscribe(counterSubscriber);  
-store.dispatch({type: 'increment'})                            
+store.dispatch({type: 'increment'});      
+store.dispatch({type : 'decrement'});                      
                                                             
